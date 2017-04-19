bef618d7350ecf021074e33d234b2ed81b2d719e
Shea Zhao <zhaoshea@gmail.com>
2017-04-19 16:41:54 -0700

    had to change my repo origin to be able to push to Github.

---
+++ Changing Repo Origin.R
@@ -0,0 +1,11 @@
+# changing repo origin
+
+#open shell in project & type:
+    #git remote -v
+    #git remote rm origin
+    #git remote add origin https://github.com/SheaZhao/in_class_coding_2.git
+    #(git remote add origin https://github.com/[YourUsername]/[YourRepoName].git)
+
+#then push to repo
+    #git remote add origin git@github.com:SheaZhao/in_class_coding_2.git
+    #git push -u origin master
